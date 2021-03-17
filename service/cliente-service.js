4237cc37a2723da1e875dd8f8052694ca160597f
João Victor <joaovictordjardim@gmail.com>
2021-03-17 10:10:18 -0300

    Projeto atualizado para uso de Fetch

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -16,29 +16,13 @@ const criaNovaLinha = (nome, email) => {
 const tabela = document.querySelector("[data-tabela]");
 
 const listaClientes = () => {
-  const promisse = new Promise((resolve, reject) => {
-    const http = new XMLHttpRequest();
-
-    http.open("GET", "http://localhost:3000/profile");
-
-    http.onload = () => {
-      if (http.status >= 400) {
-        reject(JSON.parse(http.response));
-      } else {
-        resolve(JSON.parse(http.response));
-      }
-    };
-
-    http.send();
+  return fetch(`http://localhost:3000/profile`).then((res) => {
+    return res.json();
   });
-  console.log(promisse);
-  return promisse;
 };
 
-listaClientes()
-  .then((data) => {
-    data.forEach((e) => {
-      tabela.appendChild(criaNovaLinha(e.nome, e.email));
-    });
-  })
-  .catch();
+listaClientes().then((data) => {
+  data.forEach((e) => {
+    tabela.appendChild(criaNovaLinha(e.nome, e.email));
+  });
+});
