49f77e51f4c46cc1723087e77132206523ab2844
João Victor <joaovictordjardim@gmail.com>
2021-03-18 09:08:02 -0300

    Metodo POST criado

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -4,6 +4,22 @@ const listaClientes = () => {
   });
 };
 
+const criaCliente = (nome, email) => {
+  return fetch(`http://localhost:3000/profile`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      nome: nome,
+      email: email,
+    }),
+  }).then((res) => {
+    return res.body;
+  });
+};
+
 export const clienteService = {
   listaClientes,
+  criaCliente,
 };
